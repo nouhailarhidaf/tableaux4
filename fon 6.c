60e924c3c10435efeafb1af517063a98f540f38e
nouhaila-rhidaf <nouhailarhidaf04@gmail.com>
2024-09-28 15:54:18 +0100

    fonction

---
+++ fon 6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int n;
+unsigned long long fact( n) {
+    if (n = 0 || n = 1) {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+int main() {
+    printf("veuillez entrer un nombre entier: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("La factorielle n'est pas definie pour les nombres negatifs.\n");
+    }
+     else {
+        printf("La factorielle de %d est: %llu\n", n, fact(n));
+    }
+
+    return 0;
+}
